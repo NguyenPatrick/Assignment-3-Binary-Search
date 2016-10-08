43896e09af92b2d9ac638290f590a2f57f0c20a7
Patrick Nguyen <patrick.nguyen@ocsbstudent.ca>
2016-10-08 10:55:36 -0400

    Added Comments + Bug Fixes

--- AssignmentThree/src/BinarySearch.java
+++ AssignmentThree/src/BinarySearch.java
@@ -1,3 +1,13 @@
+/****************************************************************************
+*
+* Created by: Patrick Nguyen
+* Created on: October 2016
+* This Program Sorts An Unsorted List of Integers Within an Array And Asks 
+* The User Which Number They Want to Find The Position of. Then It Finds
+* The Position(s) (If The Number Exists Within The Array).
+*
+****************************************************************************/
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
@@ -5,110 +15,112 @@ import java.util.Scanner;
 
 public class BinarySearch {
 	
-	// holds the value of arrays so it is easier to change if needed
-	static final int NUMBER_OF_VALUES = 250;	
-	static final int MAX_NUMBER_GENERATED = 1000;	
+	// global constants to make it easier to change values when needed
+	static final int NUMBER_OF_VALUES = 250; // number of array elements	
+	static final int MAX_NUMBER_GENERATED = 1000; // max random number generated
 	
+	
+	// function sorts the values of an int array from lowest to highest then returns to sorted array
 	public static int[] OrderNumbers(int[] unsortedArray){
 		
 		int[] sortedArray = new int[NUMBER_OF_VALUES];
-		int locationPlaceHolder = 0; 
-		int maxValue = MAX_NUMBER_GENERATED;
-		int minValue = 0;
-
-				 
+		int maxValue = MAX_NUMBER_GENERATED; // highest possible random number
+		int minValue = 0; // lowest possible random number
+		int locationPlaceHolder = 0; // to account for duplicate numbers
+			 
 		 for(int counter = 0; counter < (unsortedArray.length); counter++)
 		 {
+			 // loops through entire array to find the lowest value
 			 for(int counter2 = 0; counter2 < (unsortedArray.length); counter2++)
-	          {
-	        	  		 
-	        	 if (unsortedArray[counter2] < maxValue && unsortedArray[counter2] >= minValue)
+	         {
+				 if (unsortedArray[counter2] < maxValue && unsortedArray[counter2] >= minValue)
 			     {
 					 maxValue = unsortedArray[counter2];
 					 locationPlaceHolder = counter2;
-				 }
+				 }	        	  		 	        	
 			  }
-			 
-			  minValue = maxValue;
-			  unsortedArray[locationPlaceHolder] = MAX_NUMBER_GENERATED + 1; // fills up the space of the array in order to look for duplicates of the same number
+			 			 
+			  minValue = maxValue; // changes the lowest value
+			  // fills up an element in the array in order look for duplicates of the same number
+			  unsortedArray[locationPlaceHolder] = MAX_NUMBER_GENERATED + 1; 
 			  sortedArray[counter] = maxValue; 
-			  maxValue = MAX_NUMBER_GENERATED;			  		       	  
+			  maxValue = MAX_NUMBER_GENERATED; // resets maxValue       	  
 		}
 				
 		return sortedArray;		
 	}
 	
-	// procedure to find the position of a certain number inside the list
+	// procedure to find the position of a certain number inside the sorted list of numbers
 	public static void SearchNumber(int[] sortedArray, int numberToFind){
 		
-		int position; // for duplicates and worse case scenario, all numbers are the same
-		Boolean positionFound = false;
+		Boolean positionFound = false; // in case the numberToFind is not in the list
+		int positionOfNumber = 0;
 					
 		 for(int counter = 0; counter < (sortedArray.length); counter++)
 		 {
 			 if (sortedArray[counter] == numberToFind)
 			 {
-				 position = counter + 1;
-				 System.out.println("Your number is at position: " + position);
+				 positionOfNumber = counter + 1;
+				 System.out.println("Your Number Was Found At Position: " + positionOfNumber);
 				 positionFound = true;
 			 }				  		       	  
 		 }		
 		 
 		 if (positionFound.equals(false))
 		 {
-			 System.out.println("Your number is not on the list!");
-		 }
-		
+			 System.out.println("Your Number Could Not Be Found!");	
+		 }				 
+		 System.out.println("");	
 	}
 	
 	
 	public static void main(String args[])
 	{
-	
-        Random randomNumber = new Random();	
-        ArrayList<Integer> positionOfValues = new ArrayList<Integer>();
+		// imported components
+        Random randomNumber = new Random();		    
+	    Scanner scan = new Scanner(System.in);
+	    
+        // arrays & variables
 	    int[] randomNumberArray = new int[NUMBER_OF_VALUES];
 	    int[] orderedNumberArray = new int[NUMBER_OF_VALUES];
-	    
-	    Scanner scan = new Scanner(System.in);
 	    int numberToBeFound = 0;
 	    
-	    // randomize values
+	    // randomize values in array
 	    for(int counter = 0; counter < (randomNumberArray.length); counter++)
 		{		 
 			randomNumberArray[counter] =  randomNumber.nextInt(MAX_NUMBER_GENERATED) + 1;		
 		}
 
-		System.out.println(Arrays.toString(randomNumberArray)); // prints values in one line
+		System.out.println(Arrays.toString(randomNumberArray)); // prints unsorted array values in one line
 		 	 
-		orderedNumberArray = OrderNumbers(randomNumberArray);
-		System.out.println(Arrays.toString(orderedNumberArray));
-		 System.out.println("");
+		orderedNumberArray = OrderNumbers(randomNumberArray); // goes to function to sort values
+		System.out.println(Arrays.toString(orderedNumberArray)); // prints sorted array values in one line
+		System.out.println("");
 		
 		while (true)
 	 	{
-		 System.out.println("Enter number from 1-1000 to find its position");
-			if (scan.hasNextInt()){
+	        System.out.println("Enter Any Number From 1-1000 To Find Its Position!");
+			 
+			// restricts input to only integers
+			if (scan.hasNextInt())
+			{
 				numberToBeFound = scan.nextInt();
-				
-				if ((numberToBeFound > 0) && (numberToBeFound < MAX_NUMBER_GENERATED))
+				 
+				// restricts values to only within range of random numbers
+				if ((numberToBeFound > 0) && (numberToBeFound <= MAX_NUMBER_GENERATED))
 				{
-					SearchNumber(orderedNumberArray, numberToBeFound);
+					SearchNumber(orderedNumberArray, numberToBeFound); // procedure to find value
 				}
 				else
 				{
-					System.err.println("INVALID INPUT");
-
+			        System.out.println("INVALID INPUT! NOT AN INT FROM 1-1000!");	
 				}
-			}
+		    }		 
 			else 
 			{
-				System.err.println("INVALID INPUT");
-				break;
-			}	    	 
-
-					
-		 }
-
+				System.err.println("INVALID INPUT! NOT AN INT!");
+			    break; // or else the program runs in an infinite loop
+			}				    	 
+		}							
 	}
 }
